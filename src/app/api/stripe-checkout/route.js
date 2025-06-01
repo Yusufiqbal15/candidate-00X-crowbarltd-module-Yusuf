34b02b0acc5c43f6a6f651f01adf7564995af961
Mohammad yusuf iqbal <yusufiqbal150@gmail.com>
2025-06-01 11:12:38 +0530

    successpage add in production

--- src/app/api/stripe-checkout/route.js
+++ src/app/api/stripe-checkout/route.js
@@ -4,7 +4,7 @@ export async function GET() {
     const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
     const session = await stripe.checkout.sessions.create({
-        success_url: 'http://localhost:3000/success',
+        success_url: 'https://candidate-00-x-crowbarltd-module-yu.vercel.app/success',
         line_items: [
             {
                 price_data: {
